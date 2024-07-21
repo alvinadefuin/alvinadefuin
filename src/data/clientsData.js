ab89e1d907203348c8174783f95486956762c9c4
vin <adefuinalvin1@gmail.com>
2024-07-21 16:50:18 +0800

    Updated projects, about me and techstacks

--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,11 +1,11 @@
 // Import images
-import BootstrapImage from '../images/techtools/bootstrap.png';
 import CSSImage from '../images/techtools/css.png';
+import FirebaseImage from '../images/techtools/firebase.png';
 import FlutterImage from '../images/techtools/flutter.png';
 import HTMLImage from '../images/techtools/html.png';
 import JavaImage from '../images/techtools/java.png';
 import JavascriptImage from '../images/techtools/javascript.png';
-import PHPImage from '../images/techtools/php.png';
+import PythonImage from '../images/techtools/python.png';
 import ReactImage from '../images/techtools/react.png';
 
 export const clientsHeading = 'Technical Skills';
@@ -13,38 +13,38 @@ export const clientsHeading = 'Technical Skills';
 export const clientsData = [
 	{
 		id: 1,
-		title: 'Java',
-		img: JavaImage,
-	},
-	{
-		id: 2,
 		title: 'HTML',
 		img: HTMLImage,
 	},
 	{
-		id: 3,
+		id: 2,
 		title: 'CSS',
 		img: CSSImage,
 	},
 	{
-		id: 4,
-		title: 'PHP',
-		img: PHPImage,
-	},
-	{
-		id: 5,
-		title: 'Bootstrap',
-		img: BootstrapImage,
-	},
-	{
-		id: 6,
+		id: 3,
 		title: 'Javascript',
 		img: JavascriptImage,
 	},
 	{
-		id: 7,
+		id: 4,
 		title: 'React',
 		img: ReactImage,
+	},
+	{
+		id: 5,
+		title: 'Python',
+		img: PythonImage,
+	},
+	{
+		id: 6,
+		title: 'Java',
+		img: JavaImage,
+	},
+	{
+		id: 7,
+		title: 'Firebase',
+		img: FirebaseImage,
 	},
 	{
 		id: 8,
